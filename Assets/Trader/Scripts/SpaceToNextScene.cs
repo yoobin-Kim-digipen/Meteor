018ceed87799533979566c31fa347fddb6a19b25
sehyun park <103232638+sehyunParkk@users.noreply.github.com>
2025-11-07 16:53:27 +0900

    Update SpaceToNextScene.cs

--- Assets/Trader/Scripts/SpaceToNextScene.cs
+++ Assets/Trader/Scripts/SpaceToNextScene.cs
@@ -6,10 +6,13 @@ public class SpaceToNextScene : MonoBehaviour
     void Update()
     {
         Scene currentScene = SceneManager.GetActiveScene();
-        if (Input.GetKeyDown(KeyCode.Space) && (currentScene.name != "LocalMapScene"))
+        if (currentScene.name != "demo_caravan")
         {
-            StartCoroutine(GameManager.Instance.ReturnToLocalMapScene(currentScene.name));
-            Debug.Log("스페이스바로 씬 변경됨!");
+            if (Input.GetKeyDown(KeyCode.Space) && (currentScene.name != "LocalMapScene"))
+            {
+                StartCoroutine(GameManager.Instance.ReturnToLocalMapScene(currentScene.name));
+                Debug.Log("스페이스바로 씬 변경됨!");
+            }
         }
     }
 }
